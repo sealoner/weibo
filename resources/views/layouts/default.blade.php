4fc342321cccf033b2a279c2251b372e48b2e8da
xiefeng <xiefeng1002@gmail.com>
2017-10-19 12:17:07 +0800

    update

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,15 @@
 <!DOCTYPE html>
 <html>
-<head>
-  <title>@yield('title','Weibo')-基于Laravel5.5版本的仿微博App</title>
-</head>
-<body>
-  <h1>@yield('content')</h1>
-</body>
+  <head>
+    <title>@yield('title', 'Weibo App') - 基于Laravel5.5的仿微博App</title>
+    <link rel="stylesheet" href="/css/app.css">
+  </head>
+  <body>
+@include('layouts/_header')
+
+    <div class="container">
+      @yield('content')
+    </div>
+  </body>
 </html>
+@include('layouts/_footer')
---
+++ resources/views/users/create.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.default')
+@section('title', '注册页面')
+
+@section('content')
+<h1>注册页面</h1>
+@stop
