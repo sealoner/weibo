1949128c7c34bae2d227bc460fbc8c4400d4ae71
xiefeng <xiefeng1002@gmail.com>
2017-10-19 14:28:01 +0800

    Move to model to models folder

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,16 +1,18 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
+    //消息通知相关功能引用
     use Notifiable;
 
     /**
      * The attributes that are mass assignable.
+     *只可以操作$fillable属性中定义的字段
      *
      * @var array
      */
@@ -26,4 +28,11 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * 在Eloquent模型中借助对table属性的定义，指明当前要操作的数据表为“users”表
+     *
+     * @var string
+     */
+    protected $table = 'users';
 }
